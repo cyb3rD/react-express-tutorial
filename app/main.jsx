1f7800d91f62599ce522da65a3b3cf6be139b6c4
Nikolay Ignatyev <noilgt@gmail.com>
2016-05-30 17:00:02 +0300

    Finished scaffolding the app

--- app/main.jsx
+++ app/main.jsx
@@ -1,1 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 console.log('Output from JSX');
+
+var GroceryItemList = require('./components/groceryItemList.jsx');
+
+var initialItems = [{
+  name: "Icecream"
+},{
+  name: "Cheeseburger",
+  purchased: true
+},{
+  name: "Snarks"
+}];
+
+ReactDOM.render(
+  <GroceryItemList />,
+  document.getElementById('app')
+);
