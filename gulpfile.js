995cca7d1593bc8515acc8e035f07e42182af4ab
Nikolay Ignatyev <noilgt@gmail.com>
2016-06-07 18:32:52 +0300

    Adde gulp watchers, browser reloading

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,25 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
 
-gulp.task('live-server', function () {
+gulp.task('live-server', function() {
   var server = new liveServer('server/main.js');
   server.start();
+
+  // watch for changes & reload browser
+  gulp.watch('./.tmp/**/*', function(file) {
+    server.notify.apply(server, [file]);
+  });
+
 });
 
-gulp.task('serve', ['bundle','live-server'],  function () {
+gulp.task('serve', ['bundle','live-server', 'watch'],  function() {
   browserSync.init(null, {
       proxy: 'http://localhost:3000',
       port: 9001
   })
 });
 
-gulp.task('bundle', ['copy'], function () {
+gulp.task('bundle', ['copy'], function() {
   return browserify({
     entries: 'app/main.jsx',
     debug: true,
@@ -28,7 +34,13 @@ gulp.task('bundle', ['copy'], function () {
   .pipe(gulp.dest('./.tmp'));
 });
 
-gulp.task('copy', function () {
+gulp.task('copy', function() {
   gulp.src('app/*.css')
   .pipe(gulp.dest('./.tmp'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('./app/**/*.jsx', ['bundle']);
+  gulp.watch('./app/*.css', ['copy']);
+})
+
