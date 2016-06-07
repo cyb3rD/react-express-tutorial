de5d49e26f82a66fe2bc38d2d9407dbd4ce2b036
Nikolay Ignatyev <noilgt@gmail.com>
2016-06-07 10:41:17 +0300

    Added delete item feature

--- app/actions/groceryItemActionCreator.jsx
+++ app/actions/groceryItemActionCreator.jsx
@@ -7,5 +7,11 @@ module.exports = {
       payload: item,
       type: "grocery-item:add"
     })
+  },
+  delete: function (item) {
+    dispatcher.dispatch({
+      payload: item,
+      type: "grocery-item:delete"
+    })
   }
 }
